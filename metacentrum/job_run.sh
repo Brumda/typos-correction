e6256e3be237d1dd768af090b39408a5b4cc19d0
Martin Elias <eliasma7@fit.cvut.cz>
2025-02-09 23:33:14 +0100

    script tuning

--- metacentrum/job_run.sh
+++ metacentrum/job_run.sh
@@ -28,14 +28,26 @@ cd "$SCRATCHDIR/$PROJECT_NAME" || { echo >&2 "Failed to enter scratch directory"
 module load mambaforge
 
 echo "Creating conda environment at $(date)"
-mamba env create -p "$SCRATCHDIR/metacentrum/tmp_env" -f env.yaml || { echo >&2 "Failed to create Conda environment"; exit 1; }
-source activate "$SCRATCHDIR/metacentrum/tmp_env" || { echo >&2 "Failed to activate Conda environment"; exit 1; }
+mamba env create -p "$SCRATCHDIR/tmp_env" -f metacentrum/env.yaml || { echo >&2 "Failed to create Conda environment"; exit 1; }
+source activate "$SCRATCHDIR/tmp_env" || { echo >&2 "Failed to activate Conda environment"; exit 1; }
 echo "Environment created at $(date)"
 
-echo "Starting model execution at $(date)"
+mkdir -p "$SCRATCHDIR/tmp_env/lib/python3.13/site-packages/neuspell_data/checkpoints" || { echo >&2 "Failed to create checkpoints directory"; exit 1; }
+cp -r "$DATADIR/checkpoints/subwordbert-probwordnoise" "$SCRATCHDIR/tmp_env/lib/python3.13/site-packages/neuspell_data/checkpoints" || { echo >&2 "Failed to copy checkpoint"; exit 1; }
 
+echo "Starting model execution at $(date)"
 python test.py || { echo >&2 "Python script failed"; exit 1; }
 
-# TODO copy results
+cp "$SCRATCHDIR/$PROJECT_NAME/results.txt" "$DATADIR/results_$(date +'%d_%m_%Y').txt"
+
+source_file="$SCRATCHDIR/tmp_env/lib/python3.13/site-packages/neuspell_data/checkpoints/finetuned_model"
+if [ -e "$source_file" ]; then
+  cp -r "$source_file" "$DATADIR/models_$(date)"
+else
+  echo "Source file does not exist."
+fi
+
+
+
 
 echo "Task finished at $(date)"
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import os
+import time
 
 from neuspell import BertChecker
 
-DATA_PATH = "../data/"
+DATA_PATH = "./data/"
 checker = BertChecker(device="cuda")
 checker.from_pretrained()
 checker.model.to("cuda")
@@ -10,6 +11,12 @@ checker.model.to("cuda")
 print(next(checker.model.parameters()).device)
 print(checker.device)
 
+start_time = time.time()
 res = checker.evaluate(clean_file=os.path.join(DATA_PATH, "test_clean.txt"),
                        corrupt_file=os.path.join(DATA_PATH, "test_corrupt.txt"))
-print(res)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+with open("result.txt", "a") as f:
+    f.write(f"Execution time: {elapsed_time:.4f} seconds\n")
+    f.write(f"Result: {res}\n")
