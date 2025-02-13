29622714e93e4aebca4f8f9fc2f4a4ec8a744cf4
Martin Elias <eliasma7@fit.cvut.cz>
2025-02-13 12:33:58 +0100

    test

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from neuspell import BertChecker
 from neuspell.commons import DEFAULT_DATA_PATH
 
 gpu_name = torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU"
-wandb.init(project="neuspell", name="bert-checker",
+wandb.init(project="test", name="bert-checker",
            config={
                'GPU': gpu_name,
            })
@@ -21,10 +21,13 @@ checker.model.to("cuda")
 
 current_epoch = 1
 train_epochs = 2
+# clean_data = "test_clean.txt"
+# corrupt_data = "test_corrupt.txt"
+clean_data = "actual_testing_small.txt"
+corrupt_data = "actual_testing_small_second.txt"
 
-
-res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, "test_clean.txt"),
-                            corrupt_file=os.path.join(DATA_PATH, "test_corrupt.txt"))
+res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
+                            corrupt_file=os.path.join(DATA_PATH, corrupt_data))
 
 wandb.log({"test_accuracy": acc})
 with open("result.txt", "a") as f:
@@ -36,15 +39,15 @@ with open("result.txt", "a") as f:
 
 for epoch in range(current_epoch, train_epochs + 1):
     start_time = time.time()
-    checker.finetune(clean_file=os.path.join(DATA_PATH, "train_clean.txt"),
-                     corrupt_file=os.path.join(DATA_PATH, "train_corrupt.txt"),
+    checker.finetune(clean_file=os.path.join(DATA_PATH, clean_data),
+                     corrupt_file=os.path.join(DATA_PATH, corrupt_data),
                      n_epochs=1)
     elapsed_time = time.time() - start_time
 
     checker.from_pretrained(os.path.join(CHECKPOINT, f'epoch{epoch:02d}'))
 
-    res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, "test_clean.txt"),
-                                corrupt_file=os.path.join(DATA_PATH, "test_corrupt.txt"))
+    res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
+                                corrupt_file=os.path.join(DATA_PATH, corrupt_data))
 
     wandb.log({"test_accuracy": acc})
     with open("result.txt", "a") as f:
