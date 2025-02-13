1bc478582ea796ebf5f3eb38531a4cc1292439de
Martin Elias <eliasma7@fit.cvut.cz>
2025-02-13 14:50:37 +0100

    minor changes and fixes

--- test.py
+++ test.py
@@ -14,44 +14,49 @@ print(f"Running on: {gpu_name}")
 
 DATA_PATH = "./data/"
 CHECKPOINT = "checkpoints/subwordbert-probwordnoise/finetuned_model"
-checker = BertChecker(device="cuda")
-checker.from_pretrained()
-checker.model.to("cuda")
 
-current_epoch = 1
-train_epochs = 2
+current_epoch = 5  # epoch to be trained next
+train_epochs = 2  # how many epoch s to train
 # clean_data = "test_clean.txt"
 # corrupt_data = "test_corrupt.txt"
 clean_data = "actual_testing_small.txt"
 corrupt_data = "actual_testing_small_second.txt"
 
-res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
-                            corrupt_file=os.path.join(DATA_PATH, corrupt_data))
+checker = BertChecker(device="cuda")
 
-wandb.log({"test_accuracy": acc})
-with open("result.txt", "a") as f:
-    f.write(f"Evaluation of pretrained BERT model:")
-    f.write(f"Result: {res}\n")
-    f.write(20 * "#")
+if current_epoch > 1:
+    checker.from_pretrained(os.path.join(CHECKPOINT, f'epoch_{current_epoch - 1:02d}'))  # load previous epoch
+else:
+    checker.from_pretrained()
 
+checker.model.to("cuda")
 
+if current_epoch <= 1:
+    res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
+                                corrupt_file=os.path.join(DATA_PATH, corrupt_data))
 
-for epoch in range(current_epoch, train_epochs + 1):
+    wandb.log({"test_accuracy": acc})
+    with open("result.txt", "a") as f:
+        f.write(f"Evaluation of pretrained BERT model:\n")
+        f.write(f"Result:\n{res}\n")
+        f.write(20 * "#" + "\n")
+
+for epoch in range(current_epoch, train_epochs + current_epoch):
     start_time = time.time()
     checker.finetune(clean_file=os.path.join(DATA_PATH, clean_data),
                      corrupt_file=os.path.join(DATA_PATH, corrupt_data),
                      n_epochs=1)
     elapsed_time = time.time() - start_time
 
-    checker.from_pretrained(os.path.join(CHECKPOINT, f'epoch{epoch:02d}'))
+    checker.from_pretrained(os.path.join(CHECKPOINT, f'epoch_{epoch:02d}'))
 
     res, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
                                 corrupt_file=os.path.join(DATA_PATH, corrupt_data))
 
     wandb.log({"test_accuracy": acc})
     with open("result.txt", "a") as f:
-        f.write(f"Evaluation of BERT model after {epoch} epochs:")
-        f.write(f"Result: {res}\n")
-        f.write(20 * "#")
+        f.write(f"Evaluation of BERT model after {epoch} epochs:\n")
+        f.write(f"Result:\n{res}\n")
+        f.write(20 * "#" + "\n")
 
 wandb.finish()
