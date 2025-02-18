f3fd58d710f30549e1e29e86bd1e6ebaaa973311
Martin Elias <eliasma7@fit.cvut.cz>
2025-02-18 14:47:14 +0100

    elmo real data testing

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from neuspell import ElmosclstmChecker, BertChecker
 
 
 gpu_name = torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU"
-wandb.init(project="test", name="elmo-checker", resume="allow",
+wandb.init(project="neuspell", name="elmo-checker", resume="allow",
            config={
                'GPU': gpu_name,
            })
@@ -22,7 +22,7 @@ MODEL = {0:"subwordbert-probwordnoise",
 CHECKPOINT = f"checkpoints/{MODEL[pick_model]}/finetuned_model"
 
 current_epoch = 1  # epoch to be trained next
-train_epochs = 5  # how many epochs to train
+train_epochs = 8  # how many epochs to train
 # TODO make this into arguments
 train_clean_data = "train_clean.txt"
 train_corrupt_data = "train_corrupt.txt"
@@ -42,33 +42,31 @@ else:
 checker.model.to("cuda")
 
 if current_epoch <= 1:
-    result, prints, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
-                                corrupt_file=os.path.join(DATA_PATH, corrupt_data))
+    _, prints, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, test_clean_data),
+                                corrupt_file=os.path.join(DATA_PATH, test_corrupt_data))
 
     wandb.log({"test_accuracy": acc})
     with open("result2.txt", "a") as f:
         f.write(f"Evaluation of pretrained ELMO model:\n")
-        f.write(f"Result:\n{result}\n")
-        f.write(f"prints:\n{prints}\n")
+        f.write(f"Result:\n{prints}\n")
         f.write(20 * "#" + "\n")
 
 for epoch in range(current_epoch, train_epochs + current_epoch):
     start_time = time.time()
-    checker.finetune(clean_file=os.path.join(DATA_PATH, clean_data),
-                     corrupt_file=os.path.join(DATA_PATH, corrupt_data),
+    checker.finetune(clean_file=os.path.join(DATA_PATH, train_clean_data),
+                     corrupt_file=os.path.join(DATA_PATH, train_corrupt_data),
                      n_epochs=1)
     elapsed_time = time.time() - start_time
 
     checker.from_pretrained(os.path.join(CHECKPOINT, f'epoch_{epoch:02d}'))
 
-    result, prints, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, clean_data),
-                                corrupt_file=os.path.join(DATA_PATH, corrupt_data))
+    _, prints, acc = checker.evaluate(clean_file=os.path.join(DATA_PATH, test_clean_data),
+                                corrupt_file=os.path.join(DATA_PATH, test_corrupt_data))
 
     wandb.log({"test_accuracy": acc})
     with open("result2.txt", "a") as f:
         f.write(f"Evaluation of ELMO model after {epoch} epochs:\n")
-        f.write(f"Result:\n{result}\n")
-        f.write(f"prints:\n{prints}\n")
+        f.write(f"Result:\n{prints}\n")
         f.write(20 * "#" + "\n")
 
 wandb.finish()
