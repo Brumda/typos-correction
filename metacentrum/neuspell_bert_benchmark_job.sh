a828134cca99d0046ea048dae2c512d3bb70d931
Martin Elias <eliasma7@fit.cvut.cz>
2025-04-21 19:19:50 +0200

    finetuned version

--- metacentrum/neuspell_bert_benchmark_job.sh
+++ metacentrum/neuspell_bert_benchmark_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N BERT_Benchmark-TOK
+#PBS -N BERT_Benchmark-finetuned-TOK
 #PBS -l walltime=20:0:0
 #PBS -l select=1:ncpus=1:ngpus=1:mem=100gb:scratch_local=100gb:cluster=adan
 #PBS -m abe
@@ -48,8 +48,8 @@ mkdir -p "$SCRATCHDIR/tmp_env/lib/python$PYTHON_VERSION/site-packages/neuspell_d
 cp -r "$CHECKPOINTS" "$SCRATCHDIR/tmp_env/lib/python$PYTHON_VERSION/site-packages/neuspell_data/checkpoints" || { echo >&2 "Failed to copy checkpoint"; exit 1; }
 
 echo "Starting model benchmarking at $(date)"
-python neuspell_benchmark.py --tokenize --no_fix_spaces || { echo >&2 "Python script failed"; exit 1; }
+python neuspell_benchmark.py --finetuned --tokenize --no_fix_spaces || { echo >&2 "Python script failed"; exit 1; }
 
-cp "$SCRATCHDIR/$PROJECT_NAME/benchmark_results.txt" "$DATADIR/../benchmark_results/bert_pretrained_wo_space_correction_toks$(date '+%Y_%m_%d_%H').txt"  || { echo >&2 "Failed to results"; exit 1; }
+cp "$SCRATCHDIR/$PROJECT_NAME/benchmark_results.txt" "$DATADIR/../benchmark_results/bert_finetuned_wo_space_correction_toks$(date '+%Y_%m_%d_%H').txt"  || { echo >&2 "Failed to results"; exit 1; }
 
 echo "Task finished at $(date)"
